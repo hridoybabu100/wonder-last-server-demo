c83752bf47c8d32af74451208add94a7cb44f857
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-05-14 17:23:05 +0600

    get updated

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dns = require("node:dns").promises;
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const port = process.env.PORT
+const cors = require("cors");
+const { send } = require("node:process");
+const app = express();
 
+const port = process.env.PORT;
 
 // Adds headers: Access-Control-Allow-Origin: *
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const dns = require('node:dns').promises;
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
 const uri = process.env.MONGODB_URI;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,33 +24,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-     const db = client.db("WonderLastDev");
-     const dbCollection = db.collection("Collection");
+    const db = client.db("WonderLastDev");
+    const dbCollection = db.collection("Collection");
 
 
-
-    app.post('/admin', async(req, res) => {
-
-      const postData = req.body;
-
-      console.log('postdata', postData);
-      
-      const result = await dbCollection.insertOne(postData);
+    app.get("/destination", async(req, res) => {
+      const result = await dbCollection.find().toArray();
       res.send(result);
     })
 
+    app.post("/admin", async (req, res) => {
+      const postData = req.body;
 
+      // console.log("postdata", postData);
+
+      const result = await dbCollection.insertOne(postData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -59,11 +62,10 @@ async function run() {
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
